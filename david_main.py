98703d707ce74526ef4aaecc856f25b7a426413a
dkovalenko1 <dkovalenko1@kse.org.ua>
2024-11-22 13:51:05 +0200

    added argument

--- david_main.py
+++ david_main.py
@@ -1,1 +1,6 @@
 import argparse
+
+parser = argparse.ArgumentParser('tasks 2 and 4')
+
+parser.add_argument('-total', type=int, help='type year to get info about all medals for this year')
+year = parser.parse_args()
