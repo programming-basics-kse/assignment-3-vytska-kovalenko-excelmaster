1d63980d120a76d37e82dc188766df15f0b7f676
dkovalenko1 <dkovalenko1@kse.org.ua>
2024-11-22 17:55:13 +0200

    pretty return

--- david_main.py
+++ david_main.py
@@ -5,7 +5,6 @@ import csv
 parser = argparse.ArgumentParser('tasks 2 and 4')
 
 parser.add_argument('-total', type=int, help='type year to get info about all medals for this year')
-year = parser.parse_args()
 
 def get_data():
     with open('data.tsv') as file:
@@ -16,8 +15,10 @@ def get_data():
             rows.append(row)
     return rows, header
 
-def total_arg(year):
+def total_arg():
     rows, header = get_data()
+    year = 1964
+    # year = parser.parse_args().total
     TEAM = header.index('Team')
     MEDAL = header.index('Medal')
     YEAR = header.index('Year')
@@ -30,9 +31,13 @@ def total_arg(year):
         return 'Error, no such year'
     return team_medals
 
+def pretty_return():
+    year = parser.parse_args().total
+    temp_dict = total_arg()
+    for country, medals_dict in temp_dict.items():
+        bronze = medals_dict.get('Bronze', 0)
+        silver = medals_dict.get('Silver', 0)
+        gold = medals_dict.get('Gold', 0)
+        print(f"'{country}' - {gold} - {silver} - {bronze}")
 
-print(total_arg(1964))
-
-
-
-
+pretty_return()
