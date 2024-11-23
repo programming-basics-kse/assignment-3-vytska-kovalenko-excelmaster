65ec386581b14a4ea9583db0607f658656f97898
nvytska <nvytska@kse.org.ua>
2024-11-23 09:57:01 +0200

    edited main func by 3rd task

--- tasks.py
+++ tasks.py
@@ -93,16 +93,24 @@ def main():
     parser = argparse.ArgumentParser('Processing Olympic medalists data')
     parser.add_argument('filepath', help='Path to the data')
     group = parser.add_mutually_exclusive_group()
-    group.add_argument('--medals',nargs=2, required=True, help='Enter country (Team or NOC) and year')
+    group.add_argument('--medals', nargs=2, help='Enter country (Team or NOC) and year')
     group.add_argument('--overall', nargs='+', help='Enter one or more countries to get their overall')
     parser.add_argument('--output', help='Argument is optional: it is saving results to file')
 
-
     args = parser.parse_args()
-    country, year = args.medals
-    year = int(year)
-    result = print_medalists(args.filepath, country, year)
+    if args.medals:
+        country, year = args.medals
+        year = int(year)
+        result = print_medalists(args.filepath, country, year)
+    elif args.overall:
+        result_dict = overall_statistics(args.filepath, args.overall)
+        result = ''
+        for country, info in result_dict.items():
+            result += country + ": " + str(info) + "\n"
+        result = result.strip()
+
     print(result)
+
     if args.output:
         with open(args.output, 'w') as file:
             file.write(result)
