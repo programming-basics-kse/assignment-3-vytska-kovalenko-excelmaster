cf952d0d4a10b5857eea8f619862a759f99895de
nvytska <nvytska@kse.org.ua>
2024-11-24 19:43:21 +0200

    optimization

--- tasks.py
+++ tasks.py
@@ -1,5 +1,6 @@
-from validation import valid_medal, valid_country, valid_year
+from validation import valid_medal, valid_year
 from parser import created_parser
+from countries import COUNTRIES_SET
 
 def get_data(filepath):
     with open(filepath) as file:
@@ -127,45 +128,12 @@ def top_player(filepath, genders:list, categories:list):
 def main():
     parser = created_parser()
     args = parser.parse_args()
-    countries_set = {'Afghanistan', 'Albania', 'Algeria', 'Andorra', 'Angola', 'Antigua and Barbuda',
-                     'Argentina', 'Armenia', 'Australia', 'Austria', 'Azerbaijan', 'Bahamas',
-                     'Bahrain', 'Bangladesh', 'Barbados', 'Belarus', 'Belgium', 'Belize', 'Benin',
-                     'Bhutan', 'Bolivia', 'Bosnia and Herzegovina', 'Botswana', 'Brazil', 'Brunei',
-                     'Bulgaria', 'Burkina Faso', 'Burundi', 'Cambodia', 'Cameroon', 'Canada',
-                     'Central African Republic', 'Chad', 'Chile', 'China', 'Colombia', 'Comoros',
-                     'Costa Rica', 'Croatia', 'Cuba', 'Cyprus', 'Czech Republic', 'Czechoslovakia',
-                     'Denmark', 'Djibouti', 'Dominica', 'Dominican Republic', 'East Germany', 'England'
-                                                                                              'Ecuador', 'Egypt',
-                     'El Salvador', 'Equatorial Guinea', 'Eritrea', 'Estonia',
-                     'Ethiopia', 'Fiji', 'Finland', 'France', 'Gabon', 'Gambia', 'Georgia',
-                     'Germany', 'Ghana', 'Greece', 'Grenada', 'Guatemala', 'Guinea', 'Guyana',
-                     'Haiti', 'Honduras', 'Hungary', 'Iceland', 'India', 'Indonesia', 'Iran',
-                     'Iraq', 'Ireland', 'Israel', 'Italy', 'Jamaica', 'Japan', 'Jordan',
-                     'Kazakhstan', 'Kenya', 'Kiribati', 'Kosovo', 'Kuwait', 'Kyrgyzstan', 'Laos',
-                     'Latvia', 'Lebanon', 'Lesotho', 'Liberia', 'Libya', 'Liechtenstein',
-                     'Lithuania', 'Luxembourg', 'Madagascar', 'Malawi', 'Malaysia', 'Maldives',
-                     'Mali', 'Malta', 'Marshall Islands', 'Mauritania', 'Mauritius', 'Mexico',
-                     'Moldova', 'Monaco', 'Mongolia', 'Montenegro', 'Morocco', 'Mozambique',
-                     'Myanmar', 'Namibia', 'Nauru', 'Nepal', 'Netherlands', 'New Zealand',
-                     'Nicaragua', 'Niger', 'Nigeria', 'North Korea', 'Norway', 'Oman', 'Pakistan',
-                     'Palau', 'Palestine', 'Panama', 'Papua New Guinea', 'Paraguay', 'Peru',
-                     'Philippines', 'Poland', 'Portugal', 'Qatar', 'Romania', 'Russia', 'Rwanda',
-                     'Saint Kitts and Nevis', 'Saint Lucia', 'Saint Vincent and the Grenadines',
-                     'Samoa', 'San Marino', 'Sao Tome and Principe', 'Saudi Arabia', 'Senegal',
-                     'Serbia', 'Seychelles', 'Sierra Leone', 'Singapore', 'Slovakia', 'Slovenia',
-                     'Solomon Islands', 'Somalia', 'South Africa', 'South Korea', 'South Sudan',
-                     'Soviet Union', 'Spain', 'Sri Lanka', 'Sudan', 'Suriname', 'Sweden',
-                     'Switzerland', 'Syria', 'Tajikistan', 'Tanzania', 'Thailand', 'Togo',
-                     'Tonga', 'Trinidad and Tobago', 'Tunisia', 'Turkey', 'Turkmenistan',
-                     'Tuvalu', 'Uganda', 'Ukraine', 'United Arab Emirates', 'United States',
-                     'Uruguay', 'Uzbekistan', 'Vanuatu', 'Venezuela', 'Vietnam', 'West Germany',
-                     'Yemen', 'Yugoslavia', 'Zambia', 'Zimbabwe'}
     if args.medals:
         country, year = args.medals
         year = int(year)
-        result = print_medalists(args.filepath, country, year, countries_set)
+        result = print_medalists(args.filepath, country, year, COUNTRIES_SET)
     elif args.overall:
-        result_dict = overall_statistics(args.filepath, args.overall,countries_set)
+        result_dict = overall_statistics(args.filepath, args.overall,COUNTRIES_SET)
         result = ''
         for country, info in result_dict.items():
             result += country + ": " + str(info) + "\n"
